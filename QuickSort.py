65c78a760a4df5fa25df585a23710890a559b256
andreagioiaa <andreagioia.me@gmail.com>
2025-04-07 16:50:44 +0200

    Create QuickSort.py

---
+++ QuickSort.py
@@ -0,0 +1,45 @@
+import random
+
+def generaNumero( minimo, massimo ):
+    return random.randint(minimo,massimo)
+
+def creaVettore( n ):
+    vettore = [0] * n
+    for j in range(n):
+        vettore[j] = generaNumero(0, 100)
+    return vettore
+
+def QuickSort( A, p, q ):
+    if( p < q ):
+        r = Partition( A, p, q)
+        QuickSort( A, p, r-1 )
+        QuickSort( A, r+1, q )
+    return A
+
+def Partition(A, p, q):
+    x = A[q]
+    i = p - 1
+    for j in range(p, q):
+        if A[j] <= x:  # (corretto A[j], non A[q])
+            i += 1
+            Scambia(A, i, j)
+    Scambia(A, i + 1, q)  # posiziona il pivot al centro
+    return i + 1
+
+def Scambia(A, i, j):
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+
+def test_QuickSort( A_length, n_volte ):
+    inizio = 0
+    fine = A_length-1
+    for j in range( n_volte ):
+        vettore = creaVettore( A_length )
+        msg = "ESEMPIO " + str(j+1) + "\n"
+        msg += "VETTORE DI PARTENZA\n" + str(vettore) 
+        msg += "\nVETTORE ORDINATO\n" + str(QuickSort( vettore, inizio, fine ))
+        print( msg )
+    return 0
+
+test_QuickSort( 10, 10)
